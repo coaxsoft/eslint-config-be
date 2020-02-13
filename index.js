b73837a85682e7bb4998d358640c68c94a2dedfe
Taras Danyliuk <taras.danyluyk@coaxsoft.com>
2020-02-13 12:03:11 +0200

    change indent rule to 2 spaces

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ module.exports = {
         ],
         "indent": [
             "error",
-            4,
+            2,
             {
                 "SwitchCase": 1,
                 "ignoredNodes": ["TemplateLiteral"]
